6d53e28828c9438201c706834a8804aa79f5ccaa
sQnteri <haapaniemisanteri@gmail.com>
2026-02-13 23:31:42 +0200

    basic prompt handling and list of responses for future. added guardrails for agent

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    
+    if not os.path.isdir(target_dir):
+        return f'   Error: "{directory}" is not a directory'
+    if not valid_target_dir:
+        return f'   Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    dir_string =""
+    try:
+        for item in os.scandir(target_dir):
+            t = item.is_dir() 
+            s = item.stat().st_size
+            dir_string += f"   - {item.name}: file_size={s} bytes, is_dir={t}\n"
+    except Error:
+        return "   Error: couldn't scan directory"
+    
+    return dir_string
+
+
